69b1665dbe40ffd531e06ae4554ec14b7bc6289b
AngelinaRochon <60671312+AngelinaRochon@users.noreply.github.com>
2023-12-02 22:54:29 -0700

    Added skeleton for method calls for GUI to logic

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/gui/HardwarePopups.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/gui/HardwarePopups.java
@@ -1,102 +1,163 @@
 package com.thelocalmarketplace.software.gui;
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.util.function.Consumer;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class HardwarePopups {
 
-    public static void showScanMainScannerPopup(JFrame parentFrame) {
-        JDialog dialog = createDialog(parentFrame, "Scan Main Scanner");
-        addTextField(dialog, "Scan barcode using the main scanner:");
-        showDialog(dialog);
-    }
+	public static void showScanMainScannerPopup(JFrame parentFrame) {
+		JDialog dialog = createDialog(parentFrame, "Scan Main Scanner");
+		JTextField textField = addTextField(dialog, "Scan barcode using the main scanner:");
+		Consumer<String> onSubmit = inputText -> {
+			//TODO 
+		};
+		addSubmitButton(dialog, textField, onSubmit);
+		showDialog(dialog);
+	}
 
-    public static void showScanHandheldScannerPopup(JFrame parentFrame) {
-        JDialog dialog = createDialog(parentFrame, "Scan Handheld Scanner");
-        addTextField(dialog, "Scan barcode using the handheld scanner:");
-        showDialog(dialog);
-    }
+	public static void showScanHandheldScannerPopup(JFrame parentFrame) {
+		JDialog dialog = createDialog(parentFrame, "Scan Handheld Scanner");
+		JTextField textField = addTextField(dialog, "Scan barcode using the handheld scanner:");
+		Consumer<String> onSubmit = inputText -> {
+			//TODO 
+		};
+		addSubmitButton(dialog, textField, onSubmit);
+		showDialog(dialog);
+	}
 
-    public static void showVisualSearchPopup(JFrame parentFrame) {
-        JDialog dialog = createDialog(parentFrame, "Visual Search");
-        addTextField(dialog, "Enter item details for visual search:");
-        showDialog(dialog);
-    }
+	public static void showVisualSearchPopup(JFrame parentFrame) {
+		JDialog dialog = createDialog(parentFrame, "Visual Search");
+		JTextField textField = addTextField(dialog, "Enter item details for visual search:");
+		Consumer<String> onSubmit = inputText -> {
+			//TODO 
+		};
+		addSubmitButton(dialog, textField, onSubmit);
+		showDialog(dialog);
+	}
 
-    public static void showPluCodePopup(JFrame parentFrame) {
-        JDialog dialog = createDialog(parentFrame, "PLU Code");
-        addTextField(dialog, "Enter PLU code:");
-        showDialog(dialog);
-    }
+	public static void showPluCodePopup(JFrame parentFrame) {
+		JDialog dialog = createDialog(parentFrame, "PLU Code");
+		JTextField textField = addTextField(dialog, "Enter PLU code:");
+		Consumer<String> onSubmit = inputText -> {
+			//TODO 
+		};
+		addSubmitButton(dialog, textField, onSubmit);
+		showDialog(dialog);
+	}
 
-    public static void showAddItemToScalePopup(JFrame parentFrame) {
-        JDialog dialog = createDialog(parentFrame, "Add Item to Scale");
-        addTextField(dialog, "Place item on scale and enter weight:");
-        showDialog(dialog);
-    }
+	public static void showAddItemToScalePopup(JFrame parentFrame) {
+		JDialog dialog = createDialog(parentFrame, "Add Item to Scale");
+		JTextField textField = addTextField(dialog, "Place item on scale and enter weight:");
+		Consumer<String> onSubmit = inputText -> {
+			//TODO 
+		};
+		addSubmitButton(dialog, textField, onSubmit);
+		showDialog(dialog);
+	}
 
-    public static void showRemoveItemFromScalePopup(JFrame parentFrame) {
-        JDialog dialog = createDialog(parentFrame, "Remove Item from Scale");
-        addTextField(dialog, "Remove item from scale:");
-        showDialog(dialog);
-    }
-    
-    public static void showPayWithCreditPopup(JFrame parentFrame, boolean real) {
-        JDialog dialog = createDialog(parentFrame, "Pay With Credit");
-        addTextField(dialog, "Pay With Credit:");
-        showDialog(dialog);
-    }
-    
-    public static void showPayWithDebitPopup(JFrame parentFrame) {
-        JDialog dialog = createDialog(parentFrame, "Pay With Debit");
-        addTextField(dialog, "Pay With Debit:");
-        showDialog(dialog);
-    }
-    
-    public static void showInsertCoinPopup(JFrame parentFrame) {
-        JDialog dialog = createDialog(parentFrame, "Insert Coin");
-        addTextField(dialog, "Insert Coin:");
-        showDialog(dialog);
-    }
-    
-    public static void showInsertBanknotePopup(JFrame parentFrame) {
-        JDialog dialog = createDialog(parentFrame, "Insert Banknote");
-        addTextField(dialog, "Insert Banknote:");
-        showDialog(dialog);
-    }
-    
-    
+	public static void showRemoveItemFromScalePopup(JFrame parentFrame) {
+		JDialog dialog = createDialog(parentFrame, "Remove Item from Scale");
+		JTextField textField = addTextField(dialog, "Remove item from scale:");
+		Consumer<String> onSubmit = inputText -> {
+			//TODO 
+		};
+		addSubmitButton(dialog, textField, onSubmit);
+		showDialog(dialog);
+	}
 
+	public static void showPayWithCreditPopup(JFrame parentFrame, boolean real) {
+		JDialog dialog = createDialog(parentFrame, real ? "Pay With Real Credit" : "Pay With Fake Credit");
+		JTextField textField = addTextField(dialog, "Enter credit card details:");
+		Consumer<String> onSubmit = inputText -> {
+			//TODO 
+			if (real) {
+				//TODO 
+			} else {
+				//TODO 
+			}
+		};
+		addSubmitButton(dialog, textField, onSubmit);
+		showDialog(dialog);
+	}
 
-    // Pop-up creation logic 
+	public static void showPayWithDebitPopup(JFrame parentFrame) {
+		JDialog dialog = createDialog(parentFrame, "Pay With Debit");
+		JTextField textField = addTextField(dialog, "Enter debit card details:");
+		Consumer<String> onSubmit = inputText -> {
+			//TODO 
+		};
+		addSubmitButton(dialog, textField, onSubmit);
+		showDialog(dialog);
+	}
 
-    private static JDialog createDialog(JFrame parentFrame, String title) {
-        JDialog dialog = new JDialog(parentFrame, title, true);
-        dialog.setLayout(new BorderLayout());
-        dialog.setSize(new Dimension(300, 200)); 
-        dialog.setLocationRelativeTo(parentFrame);
-        return dialog;
-    }
+	public static void showInsertCoinPopup(JFrame parentFrame) {
+		JDialog dialog = createDialog(parentFrame, "Insert Coin");
+		JTextField textField = addTextField(dialog, "Enter coin value:");
+		Consumer<String> onSubmit = inputText -> {
+			//TODO 
+		};
+		addSubmitButton(dialog, textField, onSubmit);
+		showDialog(dialog);
+	}
 
-    private static void addTextField(JDialog dialog, String labelText) {
+	public static void showInsertBanknotePopup(JFrame parentFrame) {
+		JDialog dialog = createDialog(parentFrame, "Insert Banknote");
+		JTextField textField = addTextField(dialog, "Enter banknote value:");
+		Consumer<String> onSubmit = inputText -> {
+			//TODO 
+		};
+		addSubmitButton(dialog, textField, onSubmit);
+		showDialog(dialog);
+	}
+
+	// Pop-up creation logic
+	
+	private static void addSubmitButton(JDialog dialog, JTextField textField, Consumer<String> onSubmit) {
+	    JButton submitButton = new JButton("Submit");
+	    submitButton.addActionListener(e -> {
+	        String inputText = textField.getText();
+	        onSubmit.accept(inputText); // Calls the passed consumer function with the text as input
+	        dialog.dispose();
+	    });
+	    JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+	    buttonPanel.add(submitButton);
+	    dialog.add(buttonPanel, BorderLayout.PAGE_END);
+	}
+
+	private static JDialog createDialog(JFrame parentFrame, String title) {
+		JDialog dialog = new JDialog(parentFrame, title, true);
+		dialog.setLayout(new BorderLayout());
+		dialog.setSize(new Dimension(300, 200));
+		dialog.setLocationRelativeTo(parentFrame);
+		return dialog;
+	}
+
+    private static JTextField addTextField(JDialog dialog, String labelText) {
         JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         JLabel label = new JLabel(labelText);
         label.setAlignmentX(Component.CENTER_ALIGNMENT); 
-        JPanel textFieldPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        JTextField textField = new JTextField(20); 
-        textFieldPanel.add(textField);
+        JTextField textField = new JTextField(20);
         panel.add(label);
         panel.add(Box.createVerticalStrut(5)); 
-        panel.add(textFieldPanel); 
+        panel.add(textField); 
         dialog.add(panel, BorderLayout.CENTER);
+        return textField;
     }
 
-    private static void showDialog(JDialog dialog) {
-        JButton closeButton = new JButton("Close");
-        closeButton.addActionListener(e -> dialog.dispose());
-        dialog.add(closeButton, BorderLayout.PAGE_END);
-
-        dialog.setVisible(true);
-    }
+	private static void showDialog(JDialog dialog) {
+		dialog.setVisible(true);
+	}
 }
