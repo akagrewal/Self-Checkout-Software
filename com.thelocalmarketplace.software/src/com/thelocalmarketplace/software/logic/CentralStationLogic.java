030a1f5d9b6e39cf294c2ef6544b4d7842da5a1f
wyattdeich <wyatt.deichert@ucalgary.ca>
2023-12-02 22:19:12 -0700

    Updated CentralStationLogic to include predict

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/logic/CentralStationLogic.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/logic/CentralStationLogic.java
@@ -155,6 +155,11 @@ public class CentralStationLogic {
 	public MembershipLogic membershipLogic;
 	
 	/**
+	 * Instance of logic for prediction
+	 */
+	public PredictionLogic predictionLogic;
+	
+	/**
 	 * Current selected payment method
 	 */
 	private PaymentMethods paymentMethod;	
@@ -194,6 +199,7 @@ public class CentralStationLogic {
 		this.attendantLogic = new AttendantLogic(this);
 		this.addBagsLogic = new AddBagsLogic(this);
 		this.removeItemLogic = new RemoveItemLogic(this);
+		this.predictionLogic = new PredictionLogic(this);
 		
 		this.coinCurrencyLogic = new CurrencyLogic(this.hardware.getCoinDenominations());
 		this.banknoteCurrencyLogic = new CurrencyLogic(this.hardware.getBanknoteDenominations());
