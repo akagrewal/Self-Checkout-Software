a2884bcabed792dc8fa3dcbde96fd635e78e02bd
Arthur-Huan <c.arthurh@outlook.com>
2023-12-05 00:35:24 -0700

    Revert "Add group members comment"
    This reverts commit b5235028896696904dfbf088515aa8ffc90ade7c.

--- com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/controllers/AddOwnBagsTests.java
+++ com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/controllers/AddOwnBagsTests.java
@@ -1,31 +1,3 @@
-/** SENG300 Group Project
- * (Adapted from Project Iteration 2 - Group 5)
- *
- * Iteration 3 - Group 3
- * @author Jaimie Marchuk - 30112841
- * @author Wyatt Deichert - 30174611
- * @author Jane Magai - 30180119
- * @author Enzo Mutiso - 30182555
- * @author Mauricio Murillo - 30180713
- * @author Ahmed Ibrahim Mohamed Seifeldin Hassan - 30174024
- * @author Aryaman Sandhu - 30017164
- * @author Nikki Kim - 30189188
- * @author Jayden Ma - 30184996
- * @author Braden Beler - 30084941
- * @author Danish Sharma - 30172600
- * @author Angelina Rochon - 30087177
- * @author Amira Wishah - 30182579
- * @author Walija Ihsan - 30172565
- * @author Hannah Pohl - 30173027
- * @author Akashdeep Grewal - 30179657
- * @author Rhett Bramfield - 30170520
- * @author Arthur Huan - 30197354
- * @author Jaden Myers - 30152504
- * @author Jincheng Li - 30172907
- * @author Anandita Mahika - 30097559
- */
-
-
 package com.thelocalmarketplace.software.test.controllers;
 
 import static org.junit.Assert.assertTrue;
